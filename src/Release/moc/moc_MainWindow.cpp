94d835d4f9ebc3b8c5c0da209ffb1de450674794
Sahil Singh <sahils@nvidia.com>
2020-05-11 01:49:01 +0530

    1. x64 version of binry working 2. Solution migrated to VS2012
    Testing done
    window Detective_x64_Release/Debug:
    Able to log Firefox x64 messages
    Not able to log beyond compare x86 messages
    
    window Detective_win32_Release/Debug:
    Not able to log Firefox x64 messages
    Able to log beyond compare x86 messages

---
+++ src/Release/moc/moc_MainWindow.cpp
@@ -0,0 +1,261 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'MainWindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.1.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../ui/MainWindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'MainWindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.1.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[33];
+    char stringdata[456];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 17),
+QT_MOC_LITERAL(2, 29, 0),
+QT_MOC_LITERAL(3, 30, 15),
+QT_MOC_LITERAL(4, 46, 14),
+QT_MOC_LITERAL(5, 61, 20),
+QT_MOC_LITERAL(6, 82, 15),
+QT_MOC_LITERAL(7, 98, 5),
+QT_MOC_LITERAL(8, 104, 15),
+QT_MOC_LITERAL(9, 120, 16),
+QT_MOC_LITERAL(10, 137, 7),
+QT_MOC_LITERAL(11, 145, 8),
+QT_MOC_LITERAL(12, 154, 12),
+QT_MOC_LITERAL(13, 167, 3),
+QT_MOC_LITERAL(14, 171, 13),
+QT_MOC_LITERAL(15, 185, 18),
+QT_MOC_LITERAL(16, 204, 8),
+QT_MOC_LITERAL(17, 213, 6),
+QT_MOC_LITERAL(18, 220, 16),
+QT_MOC_LITERAL(19, 237, 15),
+QT_MOC_LITERAL(20, 253, 18),
+QT_MOC_LITERAL(21, 272, 7),
+QT_MOC_LITERAL(22, 280, 20),
+QT_MOC_LITERAL(23, 301, 15),
+QT_MOC_LITERAL(24, 317, 10),
+QT_MOC_LITERAL(25, 328, 18),
+QT_MOC_LITERAL(26, 347, 13),
+QT_MOC_LITERAL(27, 361, 20),
+QT_MOC_LITERAL(28, 382, 16),
+QT_MOC_LITERAL(29, 399, 8),
+QT_MOC_LITERAL(30, 408, 19),
+QT_MOC_LITERAL(31, 428, 15),
+QT_MOC_LITERAL(32, 444, 10)
+    },
+    "MainWindow\0refreshWindowTree\0\0"
+    "openPreferences\0openFindDialog\0"
+    "openSystemInfoDialog\0treeViewChanged\0"
+    "index\0treeItemChanged\0QTreeWidgetItem*\0"
+    "current\0previous\0showTreeMenu\0pos\0"
+    "updateMdiMenu\0setActiveMdiWindow\0"
+    "QWidget*\0window\0stayOnTopChanged\0"
+    "shouldStayOnTop\0locateWindowInTree\0"
+    "Window*\0viewWindowProperties\0"
+    "PropertiesPane*\0WindowList\0"
+    "viewWindowMessages\0MessagesPane*\0"
+    "editWindowProperties\0editWindowStyles\0"
+    "showLogs\0notificationTimeout\0"
+    "showAboutDialog\0launchHelp\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      21,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  119,    2, 0x0a,
+       3,    0,  120,    2, 0x0a,
+       4,    0,  121,    2, 0x0a,
+       5,    0,  122,    2, 0x0a,
+       6,    1,  123,    2, 0x0a,
+       8,    2,  126,    2, 0x0a,
+      12,    1,  131,    2, 0x0a,
+      14,    0,  134,    2, 0x0a,
+      15,    1,  135,    2, 0x0a,
+      18,    1,  138,    2, 0x0a,
+      20,    1,  141,    2, 0x0a,
+      22,    1,  144,    2, 0x0a,
+      22,    1,  147,    2, 0x0a,
+      25,    1,  150,    2, 0x0a,
+      25,    1,  153,    2, 0x0a,
+      27,    1,  156,    2, 0x0a,
+      28,    1,  159,    2, 0x0a,
+      29,    0,  162,    2, 0x0a,
+      30,    0,  163,    2, 0x0a,
+      31,    0,  164,    2, 0x0a,
+      32,    0,  165,    2, 0x0a,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    7,
+    QMetaType::Void, 0x80000000 | 9, 0x80000000 | 9,   10,   11,
+    QMetaType::Void, QMetaType::QPoint,   13,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 16,   17,
+    QMetaType::Void, QMetaType::Bool,   19,
+    QMetaType::Void, 0x80000000 | 21,    2,
+    0x80000000 | 23, 0x80000000 | 21,    2,
+    QMetaType::Void, 0x80000000 | 24,    2,
+    0x80000000 | 26, 0x80000000 | 21,    2,
+    QMetaType::Void, 0x80000000 | 24,    2,
+    QMetaType::Void, 0x80000000 | 21,    2,
+    QMetaType::Void, 0x80000000 | 21,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        switch (_id) {
+        case 0: _t->refreshWindowTree(); break;
+        case 1: _t->openPreferences(); break;
+        case 2: _t->openFindDialog(); break;
+        case 3: _t->openSystemInfoDialog(); break;
+        case 4: _t->treeViewChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: _t->treeItemChanged((*reinterpret_cast< QTreeWidgetItem*(*)>(_a[1])),(*reinterpret_cast< QTreeWidgetItem*(*)>(_a[2]))); break;
+        case 6: _t->showTreeMenu((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 7: _t->updateMdiMenu(); break;
+        case 8: _t->setActiveMdiWindow((*reinterpret_cast< QWidget*(*)>(_a[1]))); break;
+        case 9: _t->stayOnTopChanged((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 10: _t->locateWindowInTree((*reinterpret_cast< Window*(*)>(_a[1]))); break;
+        case 11: { PropertiesPane* _r = _t->viewWindowProperties((*reinterpret_cast< Window*(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< PropertiesPane**>(_a[0]) = _r; }  break;
+        case 12: _t->viewWindowProperties((*reinterpret_cast< WindowList(*)>(_a[1]))); break;
+        case 13: { MessagesPane* _r = _t->viewWindowMessages((*reinterpret_cast< Window*(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< MessagesPane**>(_a[0]) = _r; }  break;
+        case 14: _t->viewWindowMessages((*reinterpret_cast< WindowList(*)>(_a[1]))); break;
+        case 15: _t->editWindowProperties((*reinterpret_cast< Window*(*)>(_a[1]))); break;
+        case 16: _t->editWindowStyles((*reinterpret_cast< Window*(*)>(_a[1]))); break;
+        case 17: _t->showLogs(); break;
+        case 18: _t->notificationTimeout(); break;
+        case 19: _t->showAboutDialog(); break;
+        case 20: _t->launchHelp(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 8:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QWidget* >(); break;
+            }
+            break;
+        case 10:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< Window* >(); break;
+            }
+            break;
+        case 11:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< Window* >(); break;
+            }
+            break;
+        case 13:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< Window* >(); break;
+            }
+            break;
+        case 15:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< Window* >(); break;
+            }
+            break;
+        case 16:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< Window* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    if (!strcmp(_clname, "LogListener"))
+        return static_cast< LogListener*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 21)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 21;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 21)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 21;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
